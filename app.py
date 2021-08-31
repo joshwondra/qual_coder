49f9a0d850ba20e7fd3e329efef8ef17ae3e06ac
joshwondra <joshwondra@gmail.com>
2021-08-31 17:21:52 -0400

    Insert data in table * modify table creation to keep unique rows only * clean table name and column names to avoid SQL injection attacks * add cursor to execute SQL commands * add insertion of data from the csv to the table * commit changes to the SQL table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import csv
 import sqlite3
+import re
 
 app = Flask(__name__)
 
@@ -20,21 +21,46 @@ def index():
         data.save(data.filename)
         #codes.save("codes_" + codes.filename)
 
-        # open sqlite conection
+        # open sqlite conection and create cursor
         conn = sqlite3.connect("qual_data.db")
+        cursor = conn.cursor()
 
         # read in data and create table
         with open(data.filename) as file:
+
+            # read in data
             reader = csv.reader(file)
             header = next(reader)
-            tablename = data.filename.lower().replace('.csv', '').replace(' ', '_')
-            colnames = [colname + " TEXT" for colname in header]
 
-            conn.execute("CREATE TABLE IF NOT EXISTS {tablename} ({columns})".format(
-                tablename = tablename,
-                columns = ', '.join(colnames)
+            # get table name
+            tablename_init = data.filename.lower().replace('.csv', '') # set to lower case and get rid of the .csv ending
+            tablename = re.sub(r'[^\w]', '_', tablename_init) # change punctuation to _ to reduce risk of SQL injection attacks
+
+            # get column names
+            colnames_init = [re.sub(r'[^\w]', '_', colname) for colname in header] # change punctuation to _ to reduce risk of SQL injection attacks 
+            colname_type = ', '.join([colname + " TEXT" for colname in colnames_init]) # prep column names with data type
+            colnames = colname_type.replace(" TEXT", "") # remove data type
+
+            # create an empty table if it doesn't exist
+            create_query = "CREATE TABLE IF NOT EXISTS {tablename} ({colname_type}, UNIQUE({colnames}))".format(
+                tablename = tablename, 
+                colname_type = colname_type, 
+                colnames = colnames
                 )
-            )
+            cursor.execute(create_query)
+
+            # insert data into the table
+            insert_query = "INSERT INTO {tablename} ({colnames}) VALUES ({values})".format(
+                tablename = tablename, 
+                colnames = colnames,
+                values = ','.join('?' * len(header))
+                )
+            for row in reader:
+                cursor.execute(insert_query, row)
+
+            # finalize changes to the table
+            conn.commit()
+
         return render_template("index.html")
 
     else:
